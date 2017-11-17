e31a32eb5053dd20a3172621da369db27e1e071b
Alberto Williams <32202484+AlbertoWilliams@users.noreply.github.com>
2017-11-17 01:03:24 -0400

    Update GodelLib.h

--- Src/GodelLib.h
+++ Src/GodelLib.h
@@ -1,6 +1,34 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+#ifndef NULL
+#define NULL 0
+#endif
 #ifndef GODELLIB_H_INCLUDED
 #define GODELLIB_H_INCLUDED
 
-int Godel_Test();
+namespace Godel
+{
+	class Godel
+	{
+	public:
+		Godel(DWORD extendedStyles,
+			  LPCTSTR className,
+			  LPCTSTR windowName,
+			  DWORD windowStyles, 
+			  int defaultHorizontalPosition, 
+			  int defaultVerticalPosition, 
+			  int width,  
+			  int height, 
+			  HWND parentWindow, 
+			  HMENU classMenuUsed, 
+			  HINSTANCE instanceHandle, 
+			  LPVOID windowCreationData);
+		~Godel();
+		HINSTANCE hinst;
+		HWND hwndMain;
+	};
+}
 
 #endif // GODELLIB_H_INCLUDED
