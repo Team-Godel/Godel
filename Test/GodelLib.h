cda7f8369ee9fa7b368c08e2f9798956b6ee022b
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-11-16 20:43:21 -0500

    Add files via upload

---
+++ Test/GodelLib.h
@@ -0,0 +1,8 @@
+#ifndef GODELLIB_H_INCLUDED
+#define GODELLIB_H_INCLUDED
+
+int Godel_Test();
+
+int WinMain(HINSTANCE cetteInstance, HINSTANCE precedenteInstance, LPTSTR TEXT, int modeDaffichage);
+
+#endif // GODELLIB_H_INCLUDED
