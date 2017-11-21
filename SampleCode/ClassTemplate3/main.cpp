ab0a3fb48446e361ca4db45abda9bdf0a8ca299a
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-11-21 00:47:36 -0500

    Update main.cpp

--- SampleCode/ClassTemplate3/main.cpp
+++ SampleCode/ClassTemplate3/main.cpp
@@ -8,18 +8,18 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 {
     GDL *Gobj = new GDL;    // Create a new instance of GDL
 
-    GDL::CreateGDLWindow(hInstance, 640, 480); // Create a window in 640X480
+    Gobj->CreateGDLWindow(hInstance, 640, 480); // Create a window in 640X480
 
     BOOL bRet;
-    MSG msg;
+    MSG msg;   
 
     while((bRet = GetMessage(&msg, 0, 0, 0 )))
     {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            TranslateMessage(&msg);  // Traduction of keyboard's codes
+            DispatchMessage(&msg);   // Send the messages (events to the CALL BACK function)
     }
 
-    delete Gobj;
+    delete Gobj;   // Destroye the instance GDL
 
     return 0;
 }
