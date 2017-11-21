b92bedfef84052b8998c2d7019fc99ca8281bc4b
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-11-21 00:29:18 -0500

    Add files via upload

---
+++ SampleCode/ClassTemplate3/GodelLib.h
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <windows.h>
+
+
+
+class GDL
+{
+   public:
+     static GDL *CreateGDLWindow(HINSTANCE hInstance, int width, int height);
+     GDL() : m_hWnd(0) {}
+     ~GDL();
+
+   private:
+     static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+     static const char *m_pszClassName;
+     HWND m_hWnd;
+};
+
+
+const char *GDL::m_pszClassName = "GDL";
+
+GDL *GDL::CreateGDLWindow(HINSTANCE hInst, int width, int height)
+{
+    WNDCLASS wincl;
+
+    if(!GetClassInfo(hInst, m_pszClassName, &wincl))
+    {
+        wincl.style = 0;
+        wincl.hInstance = hInst;
+        wincl.lpszClassName = m_pszClassName;
+        wincl.lpfnWndProc = WindowProc;
+        wincl.cbClsExtra = 0;
+        wincl.cbWndExtra = 0;
+        wincl.hIcon = NULL;
+        wincl.hCursor = NULL;
+        wincl.hbrBackground = (HBRUSH)(COLOR_BTNFACE+1);
+        wincl.lpszMenuName = NULL;
+
+        if (RegisterClass(&wincl) == 0)
+        {
+            MessageBox(NULL,"The window class failed to register.","Error",0);
+            return 0;
+        }
+    }
+
+    GDL *g_vars = new GDL;
+    HWND hWnd = CreateWindow(m_pszClassName, NULL, WS_VISIBLE | WS_OVERLAPPED, 50, 50, width, height, NULL, NULL, hInst, g_vars);
+
+    if(hWnd == NULL)
+    {
+        delete g_vars;
+        MessageBox(NULL,"Problem creating the window.","Error",0);
+        return 0;
+    }
+
+    return g_vars;
+}
+
+
+LRESULT CALLBACK GDL::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        case WM_DESTROY:
+        {
+            GDL *g_vars = (GDL *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+
+            if(g_vars)
+            {
+              g_vars->m_hWnd = 0;
+            }
+            break;
+        }
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+
+GDL::~GDL()   // Destructor
+{
+   if(m_hWnd)
+   {
+    DestroyWindow( m_hWnd );
+   }
+}
