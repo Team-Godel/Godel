069dd8a4871f139957a90cf1cd83187df1401320
Alberto Williams <32202484+AlbertoWilliams@users.noreply.github.com>
2017-11-29 13:03:40 -0400

    Create main.cpp

---
+++ Src/main.cpp
@@ -0,0 +1,13 @@
+#include <GodelLib.h>
+
+
+using namespace std;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	GDL *Gobj = new GDL(hInstance);
+
+	Gobj->update();
+
+	return 0;
+}
