7d52c0e5f604687b19a6c41040610454de6dac1c
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-13 12:10:37 -0500

    Update main.cpp

--- SampleCode/ClassTemplate9/main.cpp
+++ SampleCode/ClassTemplate9/main.cpp
@@ -7,7 +7,7 @@ int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCm
 {
 	GDL *Gobj = new GDL(hInstance, "GDL Application", DEFAULT_POS, DEFAULT_POS, 640, 480);  // Create a window
 
-    Gobj->showCursor(0);  // Disable the cursor
+    	Gobj->showCursor(0);  // Disable the cursor
 
 	Gobj->update();  // Update the state of the window
 
